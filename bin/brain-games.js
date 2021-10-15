ba441b0913fdc4683af9b324fc2d05017b9d1e62
dinar <dinarmkbg@gmail.com>
2021-10-15 17:46:49 +0300

    fix imports

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
 import welcome from '../src/cli.js';
 
-<<<<<<< HEAD
-=======
-console.log('Welcome to the Brain Games!');
->>>>>>> 72e7fcea4b36e4b56918cee171a218f9efff65d0
 welcome();
--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
-<<<<<<< HEAD
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
   return name;
-=======
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
->>>>>>> 72e7fcea4b36e4b56918cee171a218f9efff65d0
 };
 
 export default welcome;
